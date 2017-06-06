ea558f11cefffacf823d5a5248bfd20e95d6c811
Patriek van Dorp <padorp@microsoft.com>
2017-06-06 13:55:52 +0200

    Add creation of Azure resources

--- simple-express/azure/create-azure-webapp.sh
+++ simple-express/azure/create-azure-webapp.sh
@@ -1,13 +1,18 @@
 #Install Azure CLI 2.0 according to https://docs.microsoft.com/en-us/cli/azure/install-azure-cli
 
+#Activate your Azure pass according to https://www.microsoftazurepass.com/Home/HowTo
+#                               -or-
+#Get started with a free trial here: https://azure.microsoft.com/en-us/free/
+
 #Log into Azure
 az login
 
+#Define your names and other attributes
 RESOURCE_GROUP=pvandorp-node #Fill in your own name
 LOCATION=westeurope
 APP_SERVICE_PLAN=pvandorp-node-plan #Fill in your own name
 APP_SERVICE_PLAN_SKU=S1 
-WEB_APP_NAME=pvandorp-node #Fill in your own name
+WEB_APP_NAME=pvandorp-node-web #Fill in your own name
 
 #Create a resource group
 az group create -n $RESOURCE_GROUP -l $LOCATION
@@ -16,5 +21,13 @@ az group create -n $RESOURCE_GROUP -l $LOCATION
 az appservice plan create -g $RESOURCE_GROUP -n $APP_SERVICE_PLAN --sku $APP_SERVICE_PLAN_SKU
 
 #Create app service web app
-az webapp -g $RESOURCE_GROUP -p $APP_SERVICE_PLAN -n $WEB_APP_NAME --runtime "node|6.2" --deployment-local-git
+az webapp create -g $RESOURCE_GROUP -p $APP_SERVICE_PLAN -n $WEB_APP_NAME --runtime "node|6.2"
 
+#Configure a local Git repo and get the URL to the repo
+GIT_URL=$(az webapp deployment source config-local-git -g $RESOURCE_GROUP -n $WEB_APP_NAME --query url --output tsv)
+
+#Add a Git remote and name it "azure"
+git remote add azure $GIT_URL
+
+#Push master branch to the "azure" remote
+git push -u azure master
