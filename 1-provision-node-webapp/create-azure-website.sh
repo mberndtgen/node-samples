6bc529093838ea975466290bcb291e01598cbb90
Manfred Berndtgen <mberndtgen@gmail.com>
2017-08-13 17:57:38 +0200

    corrections; chapter 1

--- 1-provision-node-webapp/create-azure-website.sh
+++ 1-provision-node-webapp/create-azure-website.sh
@@ -14,9 +14,9 @@ RESOURCE_GROUP=rg_mb_nodetest1 #Fill in your own name
 LOCATION=westeurope #Azure DC Amsterdam
 APP_SERVICE_PLAN=asp_mb_plantest1 #Fill in your own name
 APP_SERVICE_PLAN_SKU=S1 
-WEB_APP_NAME=web_mb_test1 #Fill in your own name
+WEB_APP_NAME=web-mb-test1 #Fill in your own name (DNS!, no underscores)
 DEPLOYMENT_USER=mbtest1
-DEPLOYMENT_PASSWORD=mbt3st1-u53r
+DEPLOYMENT_PASSWORD=*******
 
 #Create a resource group
 az group create -n $RESOURCE_GROUP -l $LOCATION
@@ -25,7 +25,12 @@ az group create -n $RESOURCE_GROUP -l $LOCATION
 az appservice plan create -g $RESOURCE_GROUP -n $APP_SERVICE_PLAN --sku $APP_SERVICE_PLAN_SKU
 
 #Create app service web app
-az webapp create -g $RESOURCE_GROUP -p $APP_SERVICE_PLAN -n $WEB_APP_NAME --runtime "node|6.10.0"
+az webapp create -g $RESOURCE_GROUP -p $APP_SERVICE_PLAN -n $WEB_APP_NAME --runtime "node|6.10"
+
+# website name will be web-mb-test1.azurewebsites.net
+
+# list all current runtimes
+# az webapp list-runtimes
 
 #Set the deployment credentials
 az webapp deployment user set --user-name $DEPLOYMENT_USER --password $DEPLOYMENT_PASSWORD
